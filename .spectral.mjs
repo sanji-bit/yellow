155445c74a68b159c41543eb24a898fbc45b10b0
guilizhiren@gmail.com <guilizhiren@gmail.com>
2026-01-12 12:19:17 +0800

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwMjg0NQ/spectral.js?branch_slug=main&token=bd7cffd5-07b4-49e0-b811-09f3f83dbf63";
+export default { extends: ruleset };
